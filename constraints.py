2a644007730af8eac8bcaee31a074f63d209e07e
omidsarem <omidsarem701@gmail.com>
2022-01-30 13:48:16 +0330

    last commit

---
+++ constraints.py
@@ -0,0 +1,3 @@
+API_KEY = "5055337665:AAEBMw6LnfJ8STHcD3gy5LipeROpCXmi14M"
+BITLY_ACCESS_TOKEN = "5d43ff3baca16af7fafe59112f38350e78ba409d"
+ngrok_auth_token = "24Fac22cEDxrFlpUmXAx4TRdWOY_4mCjirLpvnxxskhTSoona"
